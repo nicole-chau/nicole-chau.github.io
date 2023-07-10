b8ca922e60ff1cc829d5323aac66033128a7981b
Nicole Chau <nicolechau524@gmail.com>
2023-07-10 10:49:26 -0400

    initial setup

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: 
+  [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "GA-TRACKING_ID", // Google Analytics / GA
+        ],
+        pluginConfig: {
+          head: true,
+          // anonymize_ip: true,
+        },
+      }
+    },
+    "gatsby-plugin-postcss", 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png",
+      }
+    }, 
+    "gatsby-plugin-mdx", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }
+  ]
+};
