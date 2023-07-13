50b90372fcefc9cb728256412bee640e9e3d5630
Nicole Chau <nicolechau524@gmail.com>
2023-07-13 11:41:02 -0400

    updated card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,11 @@ import { Link } from "gatsby-link";
 
 const Card = ({ title, image, page }) => {
     return (
-        <div class="w-[350px] h-[250px] mx-4 my-8">
+        <div class="w-[350px] h-[250px] mx-4 my-9">
             <Link to={page} class="">
-                {/* <div class="max-w-full max-h-full w-auto h-auto m-auto"> */}
-                    <img src={image} class="max-w-full max-h-full ease-in-out duration-200 hover:scale-[1.02]"/>            
-                {/* </div> */}
+                <img src={image} class="max-w-full max-h-full ease-in-out duration-200 hover:scale-[1.02]"/>            
             </Link>
-            <p class="text-md text-black hover:text-black text-center -mt-3">{title}</p>
+            <p class="text-md text-black hover:text-black text-center mt-3">{title}</p>
         </div>
 
     )
