10cc833de6e49e34fa7f13616c7355acefb3c5fe
Nicole Chau <nicolechau524@gmail.com>
2023-07-18 11:26:55 -0400

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const Navbar = () => {
-    const linkStyle = "block py-2 pl-3 pr-4 text-black hover:text-powder-blue md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+    const [collapse, setCollapse] = useState(true)
+    const linkStyle = "block py-2 pl-3 pr-4 text-black hover:text-powder-blue md:p-0";
+
+    // const collapsedStyle = collapse ? 
+
     return (
-        <nav class="absolute right-0 px-8">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
+        <nav class="absolute right-0 px-2 z-10">
+            <div class="max-w-screen-xl flex flex-col justify-self-end mx-auto p-5">
+                <button type="button" onClick={() => setCollapse(!collapse)} class="ml-auto inline-flex items-center p-1 w-8 h-8 text-sm text-gray-500 rounded-lg md:hidden">
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="list-none flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <div class={(collapse ? `hidden ` : ``) + (`md:w-full md:block md:w-auto`)} id="navbar-default">
+                    <ul class="list-none flex flex-col p-4 md:p-0 mt-4 rounded-lg bg-white shadow md:shadow-none md:flex-row md:space-x-8 md:mt-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="/" class={linkStyle} aria-current="page">home</a>
                         </li>
