60c55c8f29534bd99fbd5f52311d746625fb76ba
Nicole Chau <nicolechau524@gmail.com>
2023-07-19 13:47:16 -0400

    traditional artworks

--- src/pages/art.js
+++ src/pages/art.js
@@ -4,28 +4,41 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import tedxpennCard from "../images/art/card-tedxpenn.png";
-import tedxpennVideo from "../images/art/tedxpenn.mp4"
+import tedxpennVideo from "../images/art/tedxpenn.mp4";
 
-import animationCard from "../images/art/card-animation.png"
-import animationVideo from "../images/art/animation.mp4"
+import animationCard from "../images/art/card-animation.png";
+import animationVideo from "../images/art/animation.mp4";
 
-import character from "../images/art/card-character.png"
+import character from "../images/art/card-character.png";
 
-import boatCard from "../images/art/card-boat.png"
-import boatVideo from "../images/art/boat-demo.mp4"
-import boatTexture from "../images/art/boat-texture.png"
-import boatCloseup from "../images/art/boat-closeup2.png"
+import boatCard from "../images/art/card-boat.png";
+import boatVideo from "../images/art/boat-demo.mp4";
+import boatTexture from "../images/art/boat-texture.png";
+import boatCloseup from "../images/art/boat-closeup2.png";
 
-import catCard from "../images/art/card-cat.png"
-import cat from "../images/art/cat.jpg"
-import bridgeCard from "../images/art/card-bridge.png"
-import bridge from "../images/art/bridge.jpg"
-import mountainCard from "../images/art/card-mountain.png"
-import mountain from "../images/art/mountains.jpg"
-import birdCard from "../images/art/card-bird.png"
-import bird from "../images/art/eagle.jpg"
-import cityCard from "../images/art/card-city.png"
-import city from "../images/art/city.jpg"
+import catCard from "../images/art/card-cat.png";
+import cat from "../images/art/cat.jpg";
+import bridgeCard from "../images/art/card-bridge.png";
+import bridge from "../images/art/bridge.jpg";
+import mountainCard from "../images/art/card-mountain.png";
+import mountain from "../images/art/mountains.jpg";
+import birdCard from "../images/art/card-bird.png";
+import bird from "../images/art/eagle.jpg";
+import cityCard from "../images/art/card-city.png";
+import city from "../images/art/city.jpg";
+
+import tornCard from "../images/art/card-torn.png";
+import torn from "../images/art/torn.jpg";
+import cityOfWealthCard from "../images/art/card-city-of-wealth.png";
+import cityOfWealth from "../images/art/city-of-wealth.jpg";
+import skullCard from "../images/art/card-skull.png";
+import skull from "../images/art/skull.jpg";
+import pastToPresentCard from "../images/art/card-past-to-present.png";
+import pastToPresent from "../images/art/past-to-present.jpg";
+import anorexiaCard from "../images/art/card-anorexia.png";
+import anorexia from "../images/art/anorexia.jpg";
+import growthCard from "../images/art/card-growth.png";
+import growth from "../images/art/growth.png"
 
 const Art = () => {
     return (
@@ -93,7 +106,7 @@ const Art = () => {
                     image={catCard}
                     skills={["Adobe Photoshop"]}
                     year="2020"
-                    modalImgs={[cat]} />
+                    description={<img src={cat} alt="cat drawing" class="px-8 py-4 max-h-[550px] m-auto clear-both"/>}/>
                 <Card
                     image={mountainCard}
                     skills={["Adobe Photoshop"]}
@@ -117,6 +130,40 @@ const Art = () => {
             </div>
             
             <h1 id="traditional" class="pt-16 px-48">traditional art</h1>
+            <div class="flex flex-wrap justify-items-start justify-center mx-8">
+                <Card
+                    image={tornCard}
+                    skills={["Charcoal", "Color Pencil"]}
+                    year="2018"
+                    modalImgs={[torn]} />
+                <Card
+                    image={skullCard}
+                    skills={["Pen", "Ink"]}
+                    year="2021"
+                    description={<img src={skull} alt="skull drawing" class="px-8 py-4 max-h-[550px] m-auto clear-both"/>}/>
+                <Card
+                    image={pastToPresentCard}
+                    subtitle="A depiction of Hong Kong's history."
+                    skills={["Paper cut by hand"]}
+                    year="2019"
+                    modalImgs={[pastToPresent]} />
+                <Card
+                    image={cityOfWealthCard}
+                    skills={["Adobe Photoshop", "Woven printed photographs"]}
+                    year="2017"
+                    modalImgs={[cityOfWealth]} />
+                <Card
+                    image={anorexiaCard}
+                    skills={["Paper cut by hand"]}
+                    year="2016"
+                    modalImgs={[anorexia]} />
+                <Card
+                    image={growthCard}
+                    skills={["Adobe Illustrator", "Laser cut", "Pen"]}
+                    year="2019"
+                    modalImgs={[growth]} />
+            </div>
+
             <Footer />
         </div>
     )
