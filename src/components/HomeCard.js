e093317c0ca44c16496744f296f93b91398bb6ff
nicole-chau <nicolechau524@gmail.com>
2023-07-21 14:50:41 -0400

    update home card size

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby-link";
 
 const HomeCard = ({ image, title, page}) => {
     return (
-        <div class="w-[230px] lg:w-[300px] mx-2 my-2">
+        <div class="w-[230px] lg:w-[280px] mx-2 my-2">
             <Link to={page} class="">
                 <img src={image} class="max-w-full max-h-full ease-in-out duration-200 hover:scale-[1.02] rounded-xl shadow"/>            
             </Link>
